3057ebb73fcbf645cc737e8b570376e0811d4e4b
makofane41 <makofane411@gmail.com>
2021-12-03 12:09:53 +0200

    setup firebase

---
+++ lib/Account_Model.dart
@@ -0,0 +1,15 @@
+class Account_Model{
+
+  String? uid;
+  String? first_name;
+  String? last_name;
+  String? email_address;
+  String? cell_no;
+  String? age;
+  
+  Account_Model({this.uid,this.first_name,this.last_name,this.email_address,this.cell_no,this.age});
+
+  
+
+
+}
